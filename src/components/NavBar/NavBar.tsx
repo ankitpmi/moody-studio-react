c13453234fead04d1a1116f927cbeb50d24f2a05
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-07-12 14:24:45 +0530

    feat(header): created header component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import clsx from 'clsx'
+import { NavLink } from 'react-router-dom';
+import styles from './NavBar.module.css'
+
+const NavLinks = [
+  'HOME',
+  'STORE',
+  'ACCESSORIES',
+  'BRAND',
+  'PAGES',
+  'ABOUT US',
+  'NEWS',
+  'CONTACT US '
+]
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className={clsx(styles.nav_container)}>
+      <hr className={clsx(styles.nav_divider)} />
+      <div className={clsx(styles.nav_link_view)}>
+          <div className={clsx(styles.row)}>
+            {
+              NavLinks.map(val => {
+                return(
+                  <NavLink className={clsx(styles.link_text)} to={'/'}>
+                    <p>{val}</p>
+                  </NavLink>
+                )
+              })
+            }
+          </div>
+      </div>
+      <hr className={clsx(styles.nav_divider)} />
+    </div>
   )
 }
 
