3e0a8f8a0d670f533d4c21370e632941b20eded5
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-08-13 17:03:42 +0530

    feat(theme): implemented theming

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,9 +3,11 @@ import React from 'react';
 const Home = React.lazy(() => import('./Home/Home'));
 const Cart = React.lazy(() => import('./Cart/Cart'));
 const Login = React.lazy(() => import('./Login/Login'));
+const ThemeExample = React.lazy(() => import('./ThemeExample/ThemeExample'))
 
 export {
   Home,
   Cart,
-  Login
+  Login,
+  ThemeExample
 }
--- src/theme/color.ts
+++ src/theme/color.ts
@@ -14,8 +14,36 @@ export const darkTheme = createTheme({
   primary: '#2980b9',
   secondary: "#27ae60",
   titleColor: '#ecf0f1',
-  subTitleColor: '#bdc3c7',
-  textColor: '#95a5a6',
+  subTitleColor: '#e6e6e6',
+  textColor: '#cccccc',
   borderColor: '#7f8c8d'
 });
 
+// Primary Color: #1a73e8 (Bright Blue)
+// Secondary Color: #ff7043 (Warm Orange)
+// Title Color: #212121 (Charcoal Black)
+// Subtitle Color: #757575 (Gray)
+// Text Color: #424242 (Dark Gray)
+// Border Color: #e0e0e0 (Light Gray)
+
+
+// Primary Color: #ffffff (White)
+// Secondary Color: #00796b (Teal)
+// Title Color: #000000 (Black)
+// Subtitle Color: #37474f (Slate Gray)
+// Text Color: #212121 (Charcoal Black)
+// Border Color: #bdbdbd (Silver)
+
+// Primary Color: #ffcc80 (Light Orange)
+// Secondary Color: #8d6e63 (Taupe)
+// Title Color: #3e2723 (Dark Brown)
+// Subtitle Color: #5d4037 (Medium Brown)
+// Text Color: #4e342e (Brown)
+// Border Color: #d7ccc8 (Beige)
+
+// Primary Color: #ffab40 (Dark Orange)
+// Secondary Color: #5d4037 (Dark Taupe)
+// Title Color: #d7ccc8 (Light Beige)
+// Subtitle Color: #bcaaa4 (Light Taupe)
+// Text Color: #d7ccc8 (Light Beige)
+// Border Color: #3e2723 (Dark Brown)
