b55b941d404ab62200335fd75ba86115200cb083
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-08-13 12:49:06 +0530

    feat(theme): working on theme implementation

---
+++ src/theme/color.ts
@@ -0,0 +1,21 @@
+import { createTheme } from "./theme";
+
+export const lightTheme = createTheme({
+  primary: '#3498db',
+  secondary: "#2ecc71",
+  titleColor: '#2c3e50',
+  subTitleColor: '#34495e',
+  textColor: '#7f8c8d',
+  borderColor: '#bdc3c7'
+});
+
+
+export const darkTheme = createTheme({
+  primary: '#2980b9',
+  secondary: "#27ae60",
+  titleColor: '#ecf0f1',
+  subTitleColor: '#bdc3c7',
+  textColor: '#95a5a6',
+  borderColor: '#7f8c8d'
+});
+
